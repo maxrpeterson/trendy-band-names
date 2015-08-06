ed5f7605eed1588c61fadf11e5d4dfd3d4fd7130
Max Peterson <petersonmaxr@gmail.com>
2015-08-06 17:36:21 -0400

    added a bunch of stuff

--- public/script/main.js
+++ public/script/main.js
@@ -16,7 +16,6 @@ window.addEventListener("load", function() {
 	var shapes = [
 		"Pyramid",
 		"Triangle",
-		"Oval",
 		"Circle",
 		"Diamond",
 		"Prism",
@@ -37,28 +36,45 @@ window.addEventListener("load", function() {
 		return ary[randIndex];
 	};
 
+	var bgImage = document.querySelector(".bg-image");
 	var genButton = document.querySelector("#generate");
 	var resultBox = document.querySelector("#result");
 
 	var generateBandName = function() {
 		var firstWord = getRandom(animals);
-		if (Math.random() > 0.2) {
-			console.log("shapes");
+		if (Math.random() > 0.25) {
 			var secondWord = getRandom(shapes);
 		} else {
-			console.log("otherStuff");
 			var secondWord = getRandom(otherStuff);
 		}
-		console.log(firstWord + " " + secondWord);
 		return firstWord + " " + secondWord;
 	};
 
+	var randomFlickr = function() {
+		var url = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=49910c2d8cdbaa3b733862122cbed59e&tags=lo-fi%2C+beach%2C+vignette&per_page=50&sort=relevance&format=json&nojsoncallback=1";
+		var ajaxGet = new XMLHttpRequest();
+		ajaxGet.onreadystatechange = function() {
+			if (ajaxGet.readyState === 4 && ajaxGet.status === 200) {
+				var responseArray = JSON.parse(ajaxGet.responseText).photos.photo;
+				var randPhoto = responseArray[Math.floor(Math.random() * responseArray.length)];
+				var imgUrl = "https://farm" + randPhoto.farm + ".staticflickr.com/" + randPhoto.server + "/" + randPhoto.id + "_" + randPhoto.secret + "_b.jpg";
+				bgImage.style.backgroundImage = "url(" + imgUrl + ")"
+			}
+		}
+		ajaxGet.open("GET", url);
+		ajaxGet.send(null);
+	}
+
 	var appendBandName = function(e) {
-		e.preventDefault();
+		// e.preventDefault();
 		var bandName = "<h1>" + generateBandName() + "</h1>";
 		resultBox.innerHTML = bandName;
+		randomFlickr();
 	};
 
 	genButton.addEventListener("click", appendBandName);
+
+	appendBandName();
+	randomFlickr();
 
 });
